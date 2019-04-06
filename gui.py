f9fbdcd2a2c6a071a9e599c50b51825f0cb5f1b8
Alex Wohlbruck <alexwohlbruck@gmail.com>
2019-04-06 00:03:12 -0400

    Add radio options for mode and start button

--- gui.py
+++ gui.py
@@ -4,8 +4,14 @@ import serial.tools.list_ports
 
 root = tkinter.Tk()
 
+# Define constants for mode selection
+MODE_RECORD = 1
+MODE_PLAYBACK = 2
+
+# Define gui state
 portname = tkinter.StringVar(root, "Select port")
-filename = tkinter.StringVar(root, "Test")
+filename = tkinter.StringVar(root, "")
+mode = tkinter.IntVar(root, 0)
 
 
 def pick_file():
@@ -28,12 +34,22 @@ def get_ports():
 
     return ports_str
 
-# Add widgets to window
-ttk.Button(root, text="Choose file", command=pick_file).pack()
-entry = ttk.Entry(root, textvariable=filename)
-entry.pack()
-ttk.Combobox(root, textvariable=portname, values=get_ports()).pack()
+def start():
+    if (mode.get() == MODE_PLAYBACK):
+        print("playback " + filename.get() + " " + portname.get())
+    elif (mode.get() == MODE_RECORD):
+        print("record " + filename.get() + " " + portname.get())
 
-filename.set("hello")
+# Add widgets to window
+ttk.Button(root, text="Choose file", command=pick_file).pack(pady=(10, 2))
+
+entry = ttk.Entry(root, textvariable=filename).pack(pady=(2, 2))
+
+ttk.Combobox(root, textvariable=portname, values=get_ports()).pack(pady=(2, 2), padx=(10, 10))
+
+ttk.Radiobutton(root, text="Record", variable=mode, value=1).pack(pady=(2, 2))
+ttk.Radiobutton(root, text="Playback", variable=mode, value=2).pack(pady=(2, 2))
+
+ttk.Button(root, text="Start", command=start).pack(pady=(2, 10))
 
 root.mainloop()
