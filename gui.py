a346a7fef55c4cf67c02efbf4229e44fcb864de5
Alex Wohlbruck <alexwohlbruck@gmail.com>
2019-04-06 00:29:19 -0400

    Add padding and input labels

--- gui.py
+++ gui.py
@@ -1,15 +1,18 @@
 import tkinter
-from tkinter import ttk, filedialog
+from tkinter import ttk, filedialog, messagebox
 import serial.tools.list_ports
 
 root = tkinter.Tk()
+ser = serial.Serial()
+ser.baudrate = 115200
+ser.bytesize = serial.EIGHTBITS
 
 # Define constants for mode selection
 MODE_RECORD = 1
 MODE_PLAYBACK = 2
 
 # Define gui state
-portname = tkinter.StringVar(root, "Select port")
+portname = tkinter.StringVar(root, "")
 filename = tkinter.StringVar(root, "")
 mode = tkinter.IntVar(root, 0)
 
@@ -35,20 +38,29 @@ def get_ports():
     return ports_str
 
 def start():
-    if (mode.get() == MODE_PLAYBACK):
-        print("playback " + filename.get() + " " + portname.get())
-    elif (mode.get() == MODE_RECORD):
-        print("record " + filename.get() + " " + portname.get())
+    opt_mode = mode.get()
+    opt_filename = filename.get()
+    opt_port = portname.get()
+
+    if (not opt_mode or not opt_filename or not opt_mode):
+        return messagebox.showwarning("Error", "Invalid input")
+
+    if (opt_mode == MODE_PLAYBACK):
+        print("playback " + opt_filename + " " + opt_port)
+    elif (opt_mode == MODE_RECORD):
+        print("record " + opt_filename + " " + opt_port)
 
 # Add widgets to window
-ttk.Button(root, text="Choose file", command=pick_file).pack(pady=(10, 2))
+ttk.Button(root, text="Choose file", command=pick_file).pack(pady=(10, 7))
 
-entry = ttk.Entry(root, textvariable=filename).pack(pady=(2, 2))
+ttk.Label(root, text="File name:").pack()
+entry = ttk.Entry(root, textvariable=filename).pack(pady=(0, 2))
 
-ttk.Combobox(root, textvariable=portname, values=get_ports()).pack(pady=(2, 2), padx=(10, 10))
+ttk.Label(root, text="Port:").pack()
+ttk.Combobox(root, textvariable=portname, values=get_ports()).pack(pady=(0, 2), padx=(10, 10))
 
-ttk.Radiobutton(root, text="Record", variable=mode, value=1).pack(pady=(2, 2))
-ttk.Radiobutton(root, text="Playback", variable=mode, value=2).pack(pady=(2, 2))
+ttk.Radiobutton(root, text="Record", variable=mode, value=1).pack(pady=(5, 2))
+ttk.Radiobutton(root, text="Playback", variable=mode, value=2).pack(pady=(2, 5))
 
 ttk.Button(root, text="Start", command=start).pack(pady=(2, 10))
 
